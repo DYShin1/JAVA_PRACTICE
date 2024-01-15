3df7193afa63f439018d9471019f8902293a3bce
daeyeong_256 <eodud3196@gmail.com>
2024-01-15 16:50:16 +0900

    조별 스터디
    각 문제 출제 후 문제 풀이.

---
+++ 00_QUIZ/untitled/src/main/java/group/exam/firstweek/Application.java
@@ -0,0 +1,36 @@
+package group.exam.firstweek;
+
+public class Application {
+    public static void main(String[] args) {
+
+        /* 설명. 1주차 그룹스터디 문제 풀이 */
+
+        /* 필기. 1번 문제 실행 */
+        Quiz1 q1 = new Quiz1();
+        q1.quiz1();
+
+        /* 필기. 2번 문제 실행 */
+        Quiz2 q2 = new Quiz2();
+        q2.quiz2();
+
+        /* 필기. 3번 문제 실행 */
+        Quiz3 q3 = new Quiz3();
+        q3.quiz3();
+
+        /* 필기. 4번 문제 실행 */
+        Quiz4 q4 = new Quiz4();
+        q4.quiz4();
+
+        /* 필기. 5번 문제 실행 */
+        Quiz5 q5 = new Quiz5();
+        q5.quiz5();
+
+        /* 필기. 6번 문제 실행 */
+        Quiz6 q6 = new Quiz6();
+        q6.quiz6();
+
+    }
+
+
+
+}
---
+++ 00_QUIZ/untitled/src/main/java/group/exam/firstweek/Quiz3.java
@@ -0,0 +1,59 @@
+package group.exam.firstweek;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Quiz3 {
+
+    /* # [문제] 가위, 바위, 보 프로그램 작성하기
+
+    - 사용자 입력과 random 메소드를 사용하여, 사용자(User)와 컴퓨터가 대결하는 가위 바위 보 게임을 만들어보자.
+
+    - 입력: [문자열] "가위", "바위" 혹은 "보"
+    - 출력: [문자열] 결과 반환 */
+    public void quiz3() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("가위 바위 보를 입력하세요.");
+
+        String rSP = sc.nextLine();
+        int rSPNum = 0;
+
+        int randNum = new Random().nextInt(3);     //랜덤으로 0, 1, 2 값을 받는다.
+
+        if("가위".equals(rSP)){
+            rSPNum = 0;                                     // 0을 가위로 정한다.
+        } else if ("바위".equals(rSP)) {
+            rSPNum = 1;                                     // 1은 바위로 정한다.
+        } else {
+            rSPNum = 2;                                     // 나머지 2는 보가 된다.
+        }
+
+        if(rSPNum == 0){                                    // 내가 가위를 냈을 경우
+            if(randNum == 0){                               // 컴퓨터도 가위를 내어 비기는 경우
+                System.out.println("비겼습니다.");
+            } else if (randNum == 1) {                      // 컴퓨터가 바위를 내어 지는 경우
+                System.out.println("졌습니다.");
+            } else {                                        // 나머지 내가 이긴 경우
+                System.out.println("이겼습니다.");
+            }
+        } else if(rSPNum == 1){
+            if(randNum == 1){
+                System.out.println("비겼습니다.");
+            } else if (randNum == 2) {
+                System.out.println("졌습니다.");
+            } else {
+                System.out.println("이겼습니다.");
+            }
+        } else {
+            if(randNum == 2){
+                System.out.println("비겼습니다.");
+            } else if (randNum == 0) {
+                System.out.println("졌습니다.");
+            } else {
+                System.out.println("이겼습니다.");
+            }
+        }
+
+    }
+}
