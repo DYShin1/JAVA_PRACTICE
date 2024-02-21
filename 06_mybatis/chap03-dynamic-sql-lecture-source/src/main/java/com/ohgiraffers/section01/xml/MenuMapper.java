1dc239cd66c419808f2accd552e752e86f3ca7c7
daeyeong_256 <eodud3196@gmail.com>
2024-02-21 16:07:37 +0900

    dynamic sql문 완료.

--- 06_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuMapper.java
+++ 06_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuMapper.java
@@ -1,6 +1,7 @@
 package com.ohgiraffers.section01.xml;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MenuMapper {
     List<MenuDTO> selectMenuByPrice(int maxPrice);
@@ -9,5 +10,11 @@ public interface MenuMapper {
 
     List<MenuDTO> searchMenuBySupCategory(SearchCriteria searchCriteria);
 
-    List<MenuDTO> searchMenuByRandomMenuCode();
+    List<MenuDTO> searchMenuByRandomMenuCode(List randomList);
+
+    List<MenuDTO> searchMenuByCodeOrSearchAll(SearchCriteria searchCriteria);
+
+    List<MenuDTO> searchMenuByNameOrCategory(Map<String, Object> criteria);
+
+    int updateMenu(Map<String, Object> criteria);
 }
--- 06_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
+++ 06_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
@@ -3,11 +3,11 @@ package com.ohgiraffers.section01.xml;
 import org.apache.ibatis.session.SqlSession;
 
 import java.util.List;
+import java.util.Map;
 
 import static com.ohgiraffers.section01.xml.Template.getSqlSession;
 
 public class MenuService {
-
     public void findMenuByPrice(int maxPrice) {
         SqlSession sqlSession = getSqlSession();
         MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
@@ -19,6 +19,7 @@ public class MenuService {
     }
 
     public void searchMenu(SearchCriteria searchCriteria) {
+
         SqlSession sqlSession = getSqlSession();
         MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
 
@@ -29,12 +30,13 @@ public class MenuService {
     }
 
     public void searchMenuBySupCategory(SearchCriteria searchCriteria) {
+
         SqlSession sqlSession = getSqlSession();
         MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
 
         List<MenuDTO> menus = mapper.searchMenuBySupCategory(searchCriteria);
 
-        if(menus != null && menus.size() > 0){
+        if(menus != null && menus.size() > 0) {
             menus.forEach(System.out::println);
         } else {
             System.out.println("DB와 연동 실패 또는 검색 결과 없음");
@@ -42,16 +44,64 @@ public class MenuService {
         sqlSession.close();
     }
 
-    public void searchMenuByRandomMenuCode() {
+    public void searchMenuByRandomMenuCode(List randomList) {
+
+        System.out.println("service 계층에서: " + randomList);
+
         SqlSession sqlSession = getSqlSession();
         MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
 
-        List<MenuDTO> menus = mapper.searchMenuByRandomMenuCode();
+        List<MenuDTO> menus = mapper.searchMenuByRandomMenuCode(randomList);
 
-        if(menus != null && menus.size() > 0){
+        if(menus != null && menus.size() > 0) {
             menus.forEach(System.out::println);
         } else {
             System.out.println("DB와 연동 실패 또는 검색 결과 없음");
+        }
+        sqlSession.close();
+    }
+
+    public void searchMenuByCodeOrSearchAll(SearchCriteria searchCriteria) {
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
+
+        List<MenuDTO> menus = mapper.searchMenuByCodeOrSearchAll(searchCriteria);
+
+        if(menus != null && menus.size() > 0) {
+            menus.forEach(System.out::println);
+        } else {
+            System.out.println("DB와 연동 실패 또는 검색 결과 없음");
+        }
+        sqlSession.close();
+    }
+
+    public void searchMenuByNameOrCategory(Map<String, Object> criteria) {
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
+
+        List<MenuDTO> menus = mapper.searchMenuByNameOrCategory(criteria);
+
+        if(menus != null && menus.size() > 0) {
+            menus.forEach(System.out::println);
+        } else {
+            System.out.println("DB와 연동 실패 또는 검색 결과 없음");
+        }
+        sqlSession.close();
+    }
+
+    public void modifyMenu(Map<String, Object> criteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
+
+        int result = mapper.updateMenu(criteria);
+
+        if(result > 0) {
+            System.out.println("메뉴 정보 변경에 성공하셨습니다.");
+            sqlSession.commit();
+        } else {
+            System.out.println("메뉴 정보 변경에 실패하셨습니다.");
+            sqlSession.rollback();
         }
         sqlSession.close();
     }
